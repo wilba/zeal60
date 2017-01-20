b7dc494f05307f4dba5d7cd85e7042e798b763b6
Ryan MacLean <ryan@ryanmaclean.com>
2017-01-20 15:45:15 -0800

    Update zeal60_keymap_rmac.bat
    Fixed spacebar LED issue (had been set to 7U by mistake).

--- keymaps/zeal60_keymap_rmac.bat
+++ keymaps/zeal60_keymap_rmac.bat
@@ -4,7 +4,7 @@ zeal60 backlight_config_set_values ^
 use_split_backspace=1 ^
 use_split_left_shift=0 ^
 use_split_right_shift=1 ^
-use_7u_spacebar=1 ^
+use_7u_spacebar=0 ^
 use_iso_enter=0 ^
 disable_when_usb_suspended=1 ^
 disable_after_timeout=10
